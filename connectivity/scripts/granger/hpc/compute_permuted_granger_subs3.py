cd24dfabaa0c0543704231186773f870abdf9863
bstavel <bstavel@berkeley.edu>
2025-07-30 10:52:04 -0700

    final perms???

--- connectivity/scripts/granger/hpc/compute_permuted_granger_subs3.py
+++ connectivity/scripts/granger/hpc/compute_permuted_granger_subs3.py
@@ -166,7 +166,7 @@ preproc_data_dir="/global/scratch/users/bstavel/pacman_ieeg/preprocessing_hpc"
 granger_dir = "/global/scratch/users/bstavel/pacman_ieeg/connectivity/scripts/granger"
 
 ## Prep lists ##
-subject_list = ['BJH051']
+subject_list = ['BJH039']
 pair_list = ['ofc_mfg', 'amyg_ofc', 'amyg_cing', 'hc_cing']
 
 # load sig pairs
