1316f86efe8303f0beb8365b50f07615de6fb69d
bstavel <bstavel@berkeley.edu>
2025-08-23 09:05:11 -0700

    additional perms

--- connectivity/scripts/granger/hpc/compute_permuted_granger_subs3.py
+++ connectivity/scripts/granger/hpc/compute_permuted_granger_subs3.py
@@ -166,7 +166,7 @@ preproc_data_dir="/global/scratch/users/bstavel/pacman_ieeg/preprocessing_hpc"
 granger_dir = "/global/scratch/users/bstavel/pacman_ieeg/connectivity/scripts/granger"
 
 ## Prep lists ##
-subject_list = ['LL10', 'LL12', 'LL13']
+subject_list = ['LL10']
 pair_list = ['mfg_cing']
 
 # load sig pairs
--- connectivity/scripts/granger/hpc/compute_permuted_granger_subs5.py
+++ connectivity/scripts/granger/hpc/compute_permuted_granger_subs5.py
@@ -166,7 +166,7 @@ preproc_data_dir="/global/scratch/users/bstavel/pacman_ieeg/preprocessing_hpc"
 granger_dir = "/global/scratch/users/bstavel/pacman_ieeg/connectivity/scripts/granger"
 
 ## Prep lists ##
-subject_list = ['LL17', 'LL19', 'SLCH002', 'BJH017']
+subject_list = ['LL12', 'LL13']
 pair_list = ['mfg_cing']
 
 # load sig pairs
